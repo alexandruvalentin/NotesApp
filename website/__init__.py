e8359204145510ecb24a1700cd248b5d0fed74b0
Alex Grapa <alexandru22valetin@gmail.com>
2024-07-24 15:07:11 +0100

    Adjust database path to be relative to the app file

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asodjaosijdas oasjdas'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    # Adjust the database path to be relative to this file
+    db_path = path.join(path.dirname(__file__), DB_NAME)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
     db.init_app(app)
 
     from .views import views
@@ -15,5 +19,11 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+        print("Created tables!")
 
     return app
